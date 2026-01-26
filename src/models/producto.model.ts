885a2b6bb97fe74b2ab9a9baa31fa479564edc5d
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-01-26 19:02:30 -0400

    Nuevos models para obtener y crear un nuevo producto para db

---
+++ src/models/producto.model.ts
@@ -0,0 +1,25 @@
+import db from '../config/database.js';
+import { Producto } from '../types/index.js';
+
+const ProductoModel = {
+
+  // Consulta 1: obtener todos los productos
+  getAll: (callback: (err: Error | null, rows: any[]) => void) => {
+    const sql = `
+      SELECT p.*, c.nombre AS categoria_nombre
+      FROM productos p
+      LEFT JOIN categorias c ON p.categoria_id = c.id
+    `;
+    db.all(sql, [], callback);
+  },
+
+  // Consulta 2: agregar un nuevo producto
+  create: (producto: Producto, callback: (err: Error | null) => void) => {
+    const {nombre, precio, stock, categoria_id} = producto;
+    const sql = "INSERT INTO productos (nombre, precio, stock, categoria_id) VALUES (?, ?, ?, ?)";
+    db.run(sql, [nombre, precio, stock, categoria_id], callback);
+  }
+
+}
+
+export default ProductoModel;
