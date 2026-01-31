7732bdf17a823c343925e1c18ec92ee500821ded
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-01-30 22:08:20 -0400

    Nuevo modelo para consultar un producto por ID

--- src/models/producto.model.ts
+++ src/models/producto.model.ts
@@ -13,7 +13,18 @@ const ProductoModel = {
     db.all(sql, [], callback);
   },
 
-  // Consulta 2: agregar un nuevo producto
+  // Consulta 2: obtener un producto por ID
+  getById: (id: number, callback: (err: Error | null, rows: Producto | undefined) => void) => {
+    const sql = `
+      SELECT p.*, c.nombre AS categoria
+      FROM productos p
+      LEFT JOIN categorias c ON p.categoria_id = c.id
+      WHERE p.id = ?
+    `;
+    db.get(sql, [id], callback);
+  },
+
+  // Consulta 3: agregar un nuevo producto
   create: (producto: Producto, callback: (err: Error | null) => void) => {
     const {nombre, precio, stock, categoria_id} = producto;
     const sql = "INSERT INTO productos (nombre, precio, stock, categoria_id) VALUES (?, ?, ?, ?)";
