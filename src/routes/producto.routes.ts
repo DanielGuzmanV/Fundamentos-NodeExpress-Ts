e6dac15d53d43478e486829cbde43a9344eca419
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-01-30 22:10:12 -0400

    Se creo la ruta para obtener por el ID

--- src/routes/producto.routes.ts
+++ src/routes/producto.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { obtenerProductos, crearProducto } from "../controllers/producto.controller.js";
+import { 
+  obtenerProductos, 
+  crearProducto,
+  productoID,
+} from "../controllers/producto.controller.js";
 import validarProducto from "../middlewares/producto.validator.js";
 
 const router = Router();
 
 // GET /productos = obtener todos los productos
 router.get('/', obtenerProductos);
+
+// GET /productos/:id = obtener un producto por su ID
+router.get('/:id', productoID)
 
 // POST /productos = guardar un nuevo producto
 router.post('/', validarProducto, crearProducto);
