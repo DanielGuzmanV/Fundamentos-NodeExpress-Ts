2343f121495e458b190c5adaaade6dc81bc8592f
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-02-03 16:25:20 -0400

    Se agrego promise y async/await a los modelos y controladores

--- src/controllers/producto.controller.ts
+++ src/controllers/producto.controller.ts
@@ -1,56 +1,63 @@
 import { Request, Response, NextFunction } from "express";
 import ProductoModel from "../models/producto.model.js";
 import { Producto } from "../types/index.js";
-import { error } from "node:console";
 
-export const obtenerProductos = (req: Request, res: Response, next: NextFunction) => {
-  ProductoModel.getAll((err, rows) => {
-    if(err) return next(err);
+// Consulta 1: Obtener todos
+export const obtenerProductos = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const rows = await ProductoModel.getAll();
 
     const mensaje = rows.length > 0
-      ? "Productos obtenidos:"
-      : "No se encontraron productos";
-    
+      ? "Productos obtenidos" : "No se encontraron productos";
+
     res.status(200).json({
       nota: mensaje,
       producto: rows
     })
-  })
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const productoID = (req: Request, res: Response, next: NextFunction) => {
-  const {id} = req.params;
-  const idNumerico = Number(id);
+// Consulta 2: Obtener producto por ID
+export const productoID = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {id} = req.params;
+    const idNumerico = Number(id);
+  
+    if(isNaN(idNumerico)) {
+      return res.status(400).json({ error: 'El ID proporcionado no es un numero valido'})
+    }
 
-  if(isNaN(idNumerico)) {
-    return res.status(400).json({ error: 'El ID proporcionado no es un numero valido'})
-  }
-
-  ProductoModel.getById(idNumerico, (err, rows) => {
-    if(err) return next(err);
-
+    const rows = await ProductoModel.getById(idNumerico);
+  
     if(!rows) {
       return res.status(404).json({ mensaje: "Producto no encontrado"});
     }
-
     res.status(200).json({
       mensaje: "Producto encontrado",
       datos: rows
-    })
-  })
+    });
+  } catch (err) {
+    next(err)
+  }
 }
 
-export const crearProducto = (req: Request, res: Response, next: NextFunction) => {
-  const datosProducto: Producto = req.body;
-
-  ProductoModel.create(datosProducto, function(err) {
-    if(err) return next(err);
-
+// Consulta 3: 
+export const crearProducto = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const datosProducto: Producto = req.body;
+    
+    await ProductoModel.create(datosProducto);
+    
     res.status(201).json({
       mensaje: "Producto guardado con exito",
       producto: datosProducto
-    })
-  }) 
+    });
+
+  } catch (err) {
+    next(err)
+  }
 }
 
 
--- src/models/producto.model.ts
+++ src/models/producto.model.ts
@@ -4,33 +4,48 @@ import { Producto } from '../types/index.js';
 const ProductoModel = {
 
   // Consulta 1: obtener todos los productos
-  getAll: (callback: (err: Error | null, rows: any[]) => void) => {
-    const sql = `
-      SELECT p.*, c.nombre AS categoria_nombre
-      FROM productos p
-      LEFT JOIN categorias c ON p.categoria_id = c.id
-    `;
-    db.all(sql, [], callback);
+  getAll: (): Promise<Producto[]> => {
+    return new Promise((resolve, reject) => {
+      const sql = `
+        SELECT p.*, c.nombre AS categoria_nombre
+        FROM productos p
+        LEFT JOIN categorias c ON p.categoria_id = c.id
+      `;
+      db.all(sql, [], (err, rows) => {
+        if(err) return reject(err);
+        resolve(rows as Producto[]);
+      });
+    });
   },
 
   // Consulta 2: obtener un producto por ID
-  getById: (id: number, callback: (err: Error | null, rows: Producto | undefined) => void) => {
-    const sql = `
-      SELECT p.*, c.nombre AS categoria
-      FROM productos p
-      LEFT JOIN categorias c ON p.categoria_id = c.id
-      WHERE p.id = ?
-    `;
-    db.get(sql, [id], callback);
+  getById:(id: number): Promise<Producto | undefined> => {
+    return new Promise((resolve, reject) => {
+      const sql = `
+        SELECT p.*, c.nombre AS categoria
+        FROM productos p
+        LEFT JOIN categorias c ON p.categoria_id = c.id
+        WHERE p.id = ? AND p.activo = 1
+      `;
+      db.get(sql, [id], (err, row) => {
+        if(err) return reject(err);
+        resolve(row as Producto);
+      });
+    });
   },
 
   // Consulta 3: agregar un nuevo producto
-  create: (producto: Producto, callback: (err: Error | null) => void) => {
-    const {nombre, precio, stock, categoria_id} = producto;
-    const sql = "INSERT INTO productos (nombre, precio, stock, categoria_id) VALUES (?, ?, ?, ?)";
-    db.run(sql, [nombre, precio, stock, categoria_id], callback);
-  }
+  create: (producto: Producto): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const {nombre, precio, stock, categoria_id} = producto;
+      const sql = "INSERT INTO productos (nombre, precio, stock, categoria_id) VALUES (?, ?, ?, ?)";
 
+      db.run(sql, [nombre, precio, stock, categoria_id], function(err) {
+        if(err) return reject(err);
+        resolve();
+      });
+    });
+  }
 }
 
 export default ProductoModel;
