182d945b7aa3a8f4087044afbe309ef0eed6683f
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-02-05 21:11:43 -0400

    Nuevos controladores usando los modelos creados

--- src/controllers/categoria.controller.ts
+++ src/controllers/categoria.controller.ts
@@ -38,3 +38,94 @@ export const crearCategoria = async (req: Request, res: Response, next: NextFunc
     next(err);
   }
 }
+
+// Consulta 3: actualizar el nombre de una categoria
+export const actualizarCategoria = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {id} = req.params;
+    const {nombre} = req.body;
+
+    if(!nombre) return res.status(400).json({ error: "El nuevo nombre es obligatorio"});
+    await CategoriaModel.update(Number(id), nombre);
+
+    res.json({
+      mensaje: "Categoria actualizada correctamente",
+      nuevo_nombre: nombre,
+    })
+
+  } catch (err) {
+    next(err);
+  }
+}
+
+// Consulta 4: Ocultar una categoria creada
+export const ocultarCategoria = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {id} = req.params;
+    await CategoriaModel.deleteLogical(Number(id));
+
+    res.json({
+      mensaje: "Categoria ocultada correctamente",
+      id: id,
+    })
+
+  } catch (err: any) {
+    if(err.message === "La categoria no existe o ya esta oculta") {
+      return res.status(400).json({error: err.message});
+    }
+    next(err);
+  }
+}
+
+// Consulta 5: Mostrar una categoria ocultada
+export const mostrarCategoria = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {id} = req.params;
+    await CategoriaModel.activarCategoria(Number(id));
+
+    res.json({
+      mensaje: "Categoria activada nuevamente",
+      id: id,
+    })
+
+  } catch (err: any) {
+    if(err.message === "La categoria ya esta activa o no existe") {
+      return res.status(400).json({error: err.message});
+    }
+    next(err);
+  }
+}
+
+// Consulta 6: Eliminar permanentemente una categoria por su ID
+export const eliminarCategoria = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {id} = req.params;
+    await CategoriaModel.deleteCategoria(Number(id));
+    res.json({
+      mensaje: "Categoria eliminada permanentemente",
+      id_Cat: id
+    })
+  } catch (err: any) {
+    if(err.message.includes("FOREIGN KEY")) {
+      return res.status(409).json({ 
+        error: "No puedes eliminarla, tiene productos asociados. Prueba ocultándola."
+      })
+    }
+    if(err.message === "No se encontro la categoria para eliminar") {
+      return res.status(404).json({error: err.message});
+    }
+  }
+}
+
+// Consulta 7: Eliminar todas las categorias
+export const vaciarTablaCat = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await CategoriaModel.deleteAll();
+    res.json({mensaje: "Se han eliminado todas las categorias"});
+  } catch (err: any) {
+    if(err.message === "No se encontraron categorias para eliminar") {
+      return res.status(404).json({error: err.message})
+    }
+    next(err);
+  }
+}
