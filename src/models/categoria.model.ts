2ae57fe10948549d5b3dc2a1545d72cc8638ce7d
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-02-05 21:03:19 -0400

    Nuevos modelos creados para la categoria

--- src/models/categoria.model.ts
+++ src/models/categoria.model.ts
@@ -2,10 +2,11 @@ import db from '../config/database.js';
 import { Categoria } from '../types/index.js';
 
 const CategoriaModel = {
+
   // Obtener todas las categorias
   getAll: (): Promise<Categoria[]> => {
     return new Promise((resolve, reject) => {
-      const sql = "SELECT * FROM categorias";
+      const sql = "SELECT * FROM categorias WHERE activo = 1";
       db.all(sql, [], (err, filas) => {
         if(err) return reject(err);
         resolve(filas as Categoria[]);
@@ -22,6 +23,77 @@ const CategoriaModel = {
         resolve();
       });
     });
+  },
+
+  // Actualizar el nombre de una categoria
+  update: (id: number, nuevoNombre: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const sql = "UPDATE categorias SET nombre = ? WHERE id = ?";
+
+      db.run(sql, [nuevoNombre, id], function(err) {
+        if(err) return reject(err);
+
+        if(this.changes === 0) {
+          return reject(new Error("No se encontro la categoria con ese ID"));
+        };
+
+        resolve();
+      });
+    });
+  },
+
+  // Oculatar una categoria
+  deleteLogical: (id: number): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const sql = "UPDATE categorias SET activo = 0 WHERE id = ? AND activo = 1";
+      
+      db.run(sql, [id], function(err) {
+        if(err) return reject(err);
+
+        if(this.changes === 0) {
+          return reject(new Error("La categoria no existe o ya esta oculta"));
+        }
+        resolve();
+      })
+    })
+  },
+
+  // Mostrar una categoria ocultada
+  activarCategoria: (id: number): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const sql = "UPDATE categorias SET activo = 1 WHERE id = ? AND activo = 0";
+
+      db.run(sql, [id], function(err) {
+        if(err) return reject(err);
+        if(this.changes === 0) return reject(new Error("La categoria ya esta activa o no existe"));
+        resolve();
+      })
+    })
+  },
+
+  // Borrar una categoria
+  deleteCategoria: (id: number): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const sql = "DELETE FROM categorias WHERE id = ?";
+      
+      db.run(sql, [id], function(err) {
+        if(err) return reject(err);
+        if(this.changes === 0) return reject(new Error("No se encontro la categoria para eliminar"));
+        resolve();
+      });
+    });
+  },
+
+  // Eliminar toda la tabla de categorias
+  deleteAll:(): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const sql = "DELETE FROM categorias";
+      db.run(sql, [], function(err) {
+        if(err) return reject(err);
+        if(this.changes === 0) return reject(new Error("No se encontraron categorias para eliminar"));
+        resolve();
+      })
+    })
   }
 }
 
