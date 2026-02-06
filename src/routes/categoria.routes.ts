8c3f380227b7913ab38808042d3eea8f6293ca1e
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-02-05 21:14:54 -0400

    Nuevas rutas creadas usando los controladores

--- src/routes/categoria.routes.ts
+++ src/routes/categoria.routes.ts
@@ -1,12 +1,35 @@
 import { Router } from "express";
-import { obtenerCategorias, crearCategoria } from "../controllers/categoria.controller.js";
+import { 
+  obtenerCategorias, 
+  crearCategoria, 
+  actualizarCategoria, 
+  ocultarCategoria, 
+  mostrarCategoria,
+  eliminarCategoria,
+  vaciarTablaCat
+} from "../controllers/categoria.controller.js";
 
 const router = Router();
+
+// Endpoint para vaciar toda la tabla de categoria
+router.delete('/delete', vaciarTablaCat);
 
 // Endpoint para obtener todas las categorias
 router.get('/', obtenerCategorias);
 
 // Endpoint para crear una nueva categoria
 router.post('/', crearCategoria);
+
+// Endpoint para actualizar el nombre de una categoria
+router.put('/:id', actualizarCategoria);
+
+// Endpoint para ocultar una categoria
+router.patch('/:id', ocultarCategoria);
+
+// Endpoint para mostrar una categoria oculta
+router.patch('/:id/restore', mostrarCategoria);
+
+// Endpoint para eliminar una categoria
+router.delete('/:id', eliminarCategoria);
 
 export default router;
