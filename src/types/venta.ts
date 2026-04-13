08729d39edbf614ecbc8fb9881a7857ce8197780
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-13 17:35:33 -0400

    Se creo los tipos de datos para Venta

---
+++ src/types/venta.ts
@@ -0,0 +1,10 @@
+export interface Venta {
+  id?: number;
+  producto_id: number;
+  usuario_id: number;
+  cantidad: number;
+  precio_unidad: number;
+  total: number;
+  fecha?: string;
+  producto_nombre?: string;
+}
