5fd2c6dce2796e56a6bda747b6043e34c364678b
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-30 09:41:31 -0400

    Se crearon los services para el usuario

---
+++ src/services/usuario.service.ts
@@ -0,0 +1,120 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import UsuarioModel from "../models/usuario.model.js";
+import { User } from "../types/user.js";
+
+const UsuarioServices = {
+
+  // Registrar un nuevo usuario:
+  registrarUsuario: async (datos: User) => {
+    const {username, password, rol} = datos;
+
+    // Validacion de negocio
+    if(!username || !password) throw new Error("MISSING_DATA");
+
+    // Encriptar la contraseña antes de enviarla
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const resultado = await UsuarioModel.create({
+      username, 
+      password: hashedPassword, 
+      rol: rol || 'vendedor'
+    })
+
+    // Retornar la informacion del usuario creado
+    return {
+      id: resultado.id,
+      username, 
+      rol: rol || 'vendedor'
+    }
+  },
+
+  // Iniciar sesion:
+  loginUsuario: async (datos: User) => {
+    const {username, password} = datos;
+
+    if(!username || !password) throw new Error("MISSING_DATA");
+
+    // Buscamos si el usuario existe
+    const usuario = await UsuarioModel.getByUsername(username);
+    if(!usuario) throw new Error("INVALID_CREDENTIALS");
+
+    // Comparar las contraseñas enviada con el hash
+    const coincide = await bcrypt.compare(password, usuario.password);
+    if(!coincide) throw new Error("INVALID_CREDENTIALS");
+
+    // Generar el Token JWT
+    const secret = process.env.JWT_SECRET || "clave_secreta_por_defecto";
+    const token = jwt.sign(
+      {
+        id: usuario.id,
+        username: usuario.username,
+        rol: usuario.rol
+      },
+      secret,
+      {expiresIn: "2h"}
+    );
+
+    // Retornnar datos del usuario y el token
+    return {
+      usuario: {
+        id: usuario.id,
+        username: usuario.username,
+        rol: usuario.rol
+      },
+      token
+    }
+  },
+
+  // Listas todos los usuarios:
+  listarUsuarios: async () => {
+    return await UsuarioModel.getAll();
+  },
+
+  // Obtener un usuario por ID
+  obtenerUsuarioPorId: async (id: string | number | undefined) => {
+    const idNum = Number(id);
+    if(isNaN(idNum)) throw new Error("NOT_FOUND_ID");
+
+    const usuario = await UsuarioModel.getById(idNum);
+    if(!usuario) throw new Error("USER_NOT_FOUND");
+
+    return usuario;
+  },
+
+  // Actualizar informacion basica
+  actualizarDatos: async (id: string | number | undefined, username: string, rol: string) => {
+    const idNum = Number(id);
+    if(isNaN(idNum)) throw new Error("NOT_FOUND_ID");
+
+    const userExiste = await UsuarioModel.getById(idNum);
+    if(!userExiste) throw new Error("USER_NOT_FOUND");
+
+    return await UsuarioModel.updateInfo(idNum, username, rol)
+  },
+
+  // Cambiar la contraseña
+  actualizarPassword: async (id: string | number | undefined, newPassword: string) => {
+    const idNum = Number(id);
+    if(isNaN(idNum)) throw new Error("NOT_FOUND_ID");
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+
+    return await UsuarioModel.updatePassword(idNum, hashedPassword);
+  },
+
+  // Eliminar un usuario
+  eliminarUsuario: async (id: string | number | undefined) => {
+    const idNum = Number(id);
+    if(isNaN(idNum)) throw new Error("NOT_FOUND_ID");
+
+    const userExiste = await UsuarioModel.getById(idNum);
+    if(!userExiste) throw new Error("USER_NOT_FOUND");
+
+    return await UsuarioModel.delete(idNum);
+  }
+}
+
+export default UsuarioServices;
