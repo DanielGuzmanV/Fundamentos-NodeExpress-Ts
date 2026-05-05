ccabf102d1fc0a67633cc4039ac68cecafab854c
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-05 11:17:03 -0400

    Se agrego el middleware para a algunas rutas de usuarios

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { loginUser, obtenerUsuarioId, obtenerUsuarios, registrarUser } from "../controllers/usuario.controller.js";
+import { authenticateToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // Rutas:
 // GET: /auth/usuarios = Obtener todos los usuarios
-router.get('/', obtenerUsuarios);
+router.get('/', authenticateToken, obtenerUsuarios);
 
 // GET: /auth/:id = obtener un usuario por ID
-router.get('/:id', obtenerUsuarioId);
+router.get('/:id', authenticateToken, obtenerUsuarioId);
 
 // POST: /auth/registrar = Registrar un nuevo usuario
 router.post('/registrar', registrarUser);
