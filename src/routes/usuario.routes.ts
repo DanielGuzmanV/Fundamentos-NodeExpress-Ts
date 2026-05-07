84bc75df876727882461d8b7db191d8950fb36a2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-07 11:13:51 -0400

    Se agrego un nuevo endpoint para eliminar un usuario con verificaciones

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { editarUsuario, loginUser, obtenerUsuarioId, obtenerUsuarios, registrarUser } from "../controllers/usuario.controller.js";
+import { 
+  editarUsuario, 
+  eliminarUsuario, 
+  loginUser, 
+  obtenerUsuarioId, 
+  obtenerUsuarios, 
+  registrarUser 
+} from "../controllers/usuario.controller.js";
 import { authenticateToken } from "../middlewares/auth.middleware.js";
 import { authorizeRole } from "../middlewares/rol.middleware.js";
 
@@ -20,5 +27,8 @@ router.post('/login', loginUser);
 
 // PUT: /auth/:id = Editar un usuario si esta logeado
 router.put('/:id', authenticateToken, editarUsuario);
+
+// DELETE /auth/:id = Eliminar usuarios, solo el admin puede realizarlo
+router.delete('/:id', authenticateToken, authorizeRole('admin'), eliminarUsuario)
 
 export default router;
