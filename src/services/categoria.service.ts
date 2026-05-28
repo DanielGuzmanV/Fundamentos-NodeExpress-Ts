fffe2f6b8b2ef804e423de2567bbfbdbd1122708
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-28 11:54:25 -0400

    Se agrego un nuevo services para buscar una categoria

--- src/services/categoria.service.ts
+++ src/services/categoria.service.ts
@@ -1,4 +1,5 @@
 import CategoriaModel from "../models/categoria.model.js";
+import { AppError } from "../utils/AppError.js";
 
 const CategoriaService = {
 
@@ -9,9 +10,21 @@ const CategoriaService = {
     return categorias;
   },
 
+  // Obtener un categoria
+  ObtenerUnCategoria: async (id: string[] | string | undefined) => {
+    const idNum = Number(id);
+    if(isNaN(idNum)) throw new AppError("ID no valido", 400);
+
+    const categoria = await CategoriaModel.getById(idNum);
+    if(!categoria) throw new AppError("Categoria no encontrada", 404);
+
+    const {nombre, activo} = categoria;
+    return {nombre, activo};
+  },
+
   // Crear nueva categoria
   crearNuevaCategoria: async(nombre: string) => {
-    if(!nombre) throw new Error("NOT_NAME");
+    if(!nombre) throw new AppError("El nombre es obligatorio", 400);
 
     return await CategoriaModel.create(nombre);
   },
@@ -20,13 +33,11 @@ const CategoriaService = {
   actualizarCategoria: async(id: string | string[] | undefined, nombre: string) => {
     const idNumerico = Number(id);
     
-    if(isNaN(idNumerico)) throw new Error("NOT_FOUND_ID")
-    if(!nombre) throw new Error("NOT_NAME");
+    if(isNaN(idNumerico)) throw new AppError("ID no valido", 404)
+    if(!nombre) throw new AppError("El nuevo nombre es obligatorio", 400);
 
     const existId = await CategoriaModel.getById(idNumerico);
-
-    if(!existId) throw new Error("CATEGORIA_NOT_FOUND");
-
+    if(!existId) throw new AppError("No se encontro la categoria con ese ID", 404);
 
     return await CategoriaModel.update(idNumerico, nombre);
   },
@@ -36,8 +47,8 @@ const CategoriaService = {
     const idNumerico = Number(id);
     const categoria = await CategoriaModel.getById(idNumerico);
 
-    if(isNaN(idNumerico)) throw new Error("NOT_FOUND_ID");
-    if(!categoria) throw new Error("CATEGORIA_NOT_FOUND");
+    if(isNaN(idNumerico)) throw new AppError("ID no valido", 404);
+    if(!categoria) throw new AppError("La categoria no existe o ya esta oculta", 400);
 
     return await CategoriaModel.deleteLogical(idNumerico);
   },
@@ -45,28 +56,27 @@ const CategoriaService = {
   // Mostrar una categoria oculta
   mostrarCategoriaId: async(id: string | string[] | undefined) => {
     const idNumerico = Number(id);
-    if(isNaN(idNumerico)) throw new Error("NOT_FOUND_ID");
+    if(isNaN(idNumerico)) throw new AppError("ID no valido", 404);
     
     const categoria = await CategoriaModel.getByIdSinFiltro(idNumerico)
-    if(!categoria) {
-      throw new Error("CATEGORIA_NOT_FOUND");
-    }
+    if(!categoria) throw new AppError("La categoria ya esta activa o no existe", 400);
+
     return await CategoriaModel.activarCategoria(idNumerico);
   },
 
   eliminarCategoriaId: async(id: string | string[] | undefined) => {
     const idNumerico = Number(id);
-    if(isNaN(idNumerico)) throw new Error("NOT_FOUND_ID");
+    if(isNaN(idNumerico)) throw new AppError("ID no valido", 404);
 
     const categoria = await CategoriaModel.getById(idNumerico);
-    if(!categoria) throw new Error("CATEGORIA_NOT_FOUND")
+    if(!categoria) throw new AppError("No se encontro la categoria para eliminar", 404)
     
     return await CategoriaModel.deleteCategoria(idNumerico);
   },
 
   eliminarTablaCat: async() => {
     const total = await CategoriaModel.countAll();
-    if(total === 0) throw new Error("NO_CATEGORIAS_FOUND");
+    if(total === 0) throw new AppError("No se encontraron categorias para eliminar", 404);
     
     return await CategoriaModel.deleteAll();
   }
