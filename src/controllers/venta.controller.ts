ff561694c1effde5f96e3383e1162d84fd7f322a
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-06-15 10:19:42 -0400

    Se crearon algunos controllers para las ventas

---
+++ src/controllers/venta.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response, NextFunction } from "express"
+import VentaService from "../services/venta.service.js"
+
+// Controller para listar todas las ventas
+export const listarVentas = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const ventas = await VentaService.listarVentas();
+
+    res.status(200).json({
+      mensaje: ventas.length > 0 ? "Lista de ventas obtenidas correctamente" : "No hay ventas registradas",
+      cantidad_ventas: ventas.length,
+      datos: ventas
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+// Controller para obtener una venta por ID
+export const obtenerVentaId = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {id} = req.params;
+    const venta = await VentaService.obtenerVentaId(id);
+
+    res.status(200).json({
+      mensaje: "Venta encontrada",
+      datos: venta
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+// Controller para crear una venta
+export const crearVenta = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {producto_id, cantidad} = req.body;
+
+    const nuevaVenta = await VentaService.crearVenta({producto_id, cantidad}, req.user!);
+
+    res.status(201).json({
+      mensaje: "Venta registrada con exito",
+      id_venta: nuevaVenta.id,
+      total_venta: nuevaVenta.total,
+      producto_id: producto_id,
+      cantidad: cantidad
+    })
+  } catch (err: any) {
+    next(err);
+  }
+}
