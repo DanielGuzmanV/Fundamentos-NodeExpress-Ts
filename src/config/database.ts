1eccdee1de3f52d2bd16890fbcc97362c92cf210
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-06-17 11:07:17 -0400

    Configuracion de la database para usar knex

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,92 +1,31 @@
-import sqlite3 from "sqlite3";
+import knex from "knex";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Configuracion de rutas para ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const dbPath = path.resolve(__dirname, '../../inventario.db');
 
-const db = new sqlite3.Database(dbPath, (err: Error | null) => {
-  if(err) {
-    console.error('Error al abrir la DB:', err.message);
-  } else {
-    console.log('Conectado a SQLite con TS');
-    db.run("PRAGMA foreign_keys = ON;");
-  }
+// Ruta del archivo para la base de datos
+const dbPath = path.resolve(__dirname, '../../inventario_knex.db');
+
+const db = knex({
+  client: "sqlite3",
+  connection: {
+    filename: dbPath,
+  },
+  useNullAsDefault: true,
+  debug: true
 });
 
-db.serialize(() => {
+// Habilitacion de las llaves foraneas para SQLite de forma nativa con knex
+db.raw("PRAGMA foreign_keys = ON;")
+  .then(() => {
+    console.log("Knex: llaves foraneas activadas con exito");
+  })
+  .catch((err) => {
+    console.log("Error al activar llaves foraneas:", err.message);
+  });
 
-  // Tabla categorias:
-  db.run(`
-      CREATE TABLE IF NOT EXISTS categorias (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT NOT NULL UNIQUE,
-        activo INTEGER DEFAULT 1
-      )
-    `, (err) => {
-      if(err) console.error("Error al crear la tabla categorias:", err.message);
-      else console.log("Tabla 'Categoria' lista");
-    }
-  );
-  
-  // Tabla de productos:
-  db.run(`
-      CREATE TABLE IF NOT EXISTS productos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT NOT NULL,
-        precio REAL NOT NULL,
-        stock INTEGER NOT NULL,
-        categoria_id INTEGER,
-        activo INTEGER DEFAULT 1,
-        FOREIGN KEY (categoria_id) REFERENCES categorias (id)
-      )
-    `, (err) => {
-      if(err) console.error("Error al crear la tabla de productos");
-      else console.log("Tabla 'Productos' lista");
-    }
-  );
-
-  // Tabla de usuarios:
-  db.run(`
-      CREATE TABLE IF NOT EXISTS usuarios (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL,
-        nombre TEXT, 
-        apellido TEXT,
-        email TEXT NULL UNIQUE,
-        telefono TEXT,
-        rol TEXT DEFAULT 'vendedor',
-        activo INTEGER DEFAULT 1,
-        fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP
-      )
-    `, (err) => {
-      if (err) console.error("Error al crear la tabla usuarios:", err.message);
-      else console.log("Tabla 'Usuarios' lista");
-    }
-  );
-
-  // Tabla de ventas:
-  db.run(`
-      CREATE TABLE IF NOT EXISTS ventas (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        producto_id INTEGER NOT NULL,
-        usuario_id INTEGER NOT NULL,
-        cantidad INTEGER DEFAULT 1,
-        precio_unidad REAL NOT NULL,
-        total REAL NOT NULL,
-        fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (producto_id) REFERENCES productos (id),
-        FOREIGN KEY (usuario_id) REFERENCES usuarios (id)
-      )
-    `, (err) => {
-      if (err) console.error("Error al crear la tabla de ventas:", err.message);
-      else console.log("Tabla 'Ventas' lista");
-    }
-  );  
-
-});
+console.log("Inicializado: Conectado a SQLite via knex.js");
 
 export default db;
