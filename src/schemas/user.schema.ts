a2fdc3d91b459d3de581e8a7ea8e4ecb9a27dc60
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-06-18 10:01:49 -0400

    Se agrego un nuevo schema para registrar un usuario

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -21,3 +21,28 @@ export const updatePasswordSchema = z.object({
   message: "La nueva contraseña debe ser diferente a la contraseña actual",
   path: ["newPassword"],
 })
+
+// Esquema para registrar un usuario
+export const registrarUsuarioSchema = z.object({
+  username: z.string()
+    .min(1, {message: "El nombre del usuario es obligatorio"}),
+  password: z.string()
+    .min(8, {message: "La contraseña debe tener al menos 8 caracteres"})
+    .regex(/[A-Z]/, { message: "La contraseña debe contener al menos una mayúscula" })
+    .regex(/[a-z]/, { message: "La contraseña debe contener al menos una minúscula" })
+    .regex(/[0-9]/, { message: "La contraseña debe contener al menos un número" })
+    .regex(/[^a-zA-Z0-9]/, { message: "La contraseña debe contener al menos un caracter especial" }),
+  nombre: z.string()
+    .min(1, { message: "El nombre es obligatorio" }),
+  apellido: z.string()
+    .min(1, { message: "El apellido es obligatorio" }),
+  email: z.string()
+    .min(1, { message: "El email es obligatorio" })
+    .email({ message: "Formato de email inválido" }),
+  telefono: z.string()
+    .min(7, { message: "El teléfono debe tener al menos 8 dígitos" })
+    .regex(/^[0-9]+$/, { message: "El teléfono solo debe contener números" }),
+  rol: z.enum(["admin", "vendedor"], {
+    message: "El rol no es valido. Debe ser 'admin' o 'vendedor'."
+  }).optional().default('vendedor'),
+})
