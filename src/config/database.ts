78c91f887b2ddd28c2cf33fca7478821d888d748
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-20 10:13:43 -0400

    Se ajusto la clave de configuracion para activar las llaves foraneas

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,6 @@
 import knex from "knex";
 import path from "path";
+import { Database } from "sqlite3";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -14,17 +15,21 @@ const db = knex({
     filename: dbPath,
   },
   useNullAsDefault: true,
-  debug: true
+  debug: true,
+  // Ajuste: asegura que cada conexion que use Express active las llaves foraneas 
+  pool: {
+    afterCreate: (conn: Database, cb: (err: Error | null) => void) => {
+      conn.run("PRAGMA foreign_keys = ON;", (err) => {
+        if(err) {
+          console.error("Error crítico: No se pudieron activar las llaves foráneas en el pool:", err.message);
+          return cb(err);
+        }
+        console.log("Knex Pool: Llaves foráneas activadas con éxito en esta conexión");
+        cb(null)
+      });
+    }
+  }
 });
-
-// Habilitacion de las llaves foraneas para SQLite de forma nativa con knex
-db.raw("PRAGMA foreign_keys = ON;")
-  .then(() => {
-    console.log("Knex: llaves foraneas activadas con exito");
-  })
-  .catch((err) => {
-    console.log("Error al activar llaves foraneas:", err.message);
-  });
 
 console.log("Inicializado: Conectado a SQLite via knex.js");
 
