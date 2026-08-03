6f3b6d6b621a3a9ba3f4f1eeb85c07383d42cce4
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-03 14:32:47 -0400

    Se creo un adaptador para prisma sqlite3

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,11 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/index.js";
 
+// Creamos el adaptador
+const adapter = new PrismaBetterSqlite3({url: "inventario_prisma.db"});
+
 const prisma = new PrismaClient({
+  adapter,
   log: ["query", "info", "warn", "error"],
 });
 
