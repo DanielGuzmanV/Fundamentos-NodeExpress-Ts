d95ec2d59d01ad2d6f4aa0d3a66fb0a6992c52e6
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-18 12:02:56 -0400

    Se refactorizaron las consultas 3 y 4 del model producto

--- src/models/producto.model.ts
+++ src/models/producto.model.ts
@@ -45,7 +45,7 @@ const ProductoModel = {
   },
 
   // Consulta 2: obtener un producto por ID
-  getByIds: async(id: number): Promise<Producto | null> => {
+  getById: async(id: number): Promise<Producto | null> => {
     return await prisma.producto.findFirst({
       where: {
         id,
@@ -57,47 +57,28 @@ const ProductoModel = {
     })
   },
 
-  // Analizar y preguntar varias cosas sobre la consulta 1 y 2
-  getById:(id: number): Promise<Producto | undefined> => {
-    return new Promise((resolve, reject) => {
-      const sql = `
-        SELECT p.*, c.nombre AS categoria
-        FROM productos p
-        LEFT JOIN categorias c ON p.categoria_id = c.id
-        WHERE p.id = ? AND p.activo = 1
-      `;
-      db.get(sql, [id], (err, row) => {
-        if(err) return reject(err);
-        resolve(row as Producto);
-      });
-    });
-  },
-
-  // Consulta para buscar producto por nombre exacto
-  getByName: (nombre: string): Promise<Producto | undefined> => {
-    return new Promise((resolve, reject) => {
-      const sql = "SELECT * FROM productos WHERE nombre = ? AND activo = 1";
-      db.get(sql, [nombre], (err, row) => {
-        if(err) return reject(err);
-        resolve(row as Producto | undefined);
-      })
+  // Consulta 3: para buscar producto por nombre exacto\
+  getByName: async(nombre: string): Promise<Producto | null> => {
+    return await prisma.producto.findFirst({
+      where: {
+        nombre: nombre,
+        activo: 1
+      }
     })
   },
 
-  // Consulta 3: agregar un nuevo producto
-  create: (producto: Producto): Promise<number> => {
-    return new Promise((resolve, reject) => {
-      const {nombre, precio, stock, categoria_id} = producto;
-      const sql = "INSERT INTO productos (nombre, precio, stock, categoria_id) VALUES (?, ?, ?, ?)";
+  // Consulta 4: agregar un nuevo producto
+  create: async(producto: Omit<Producto, 'id' | 'activo'>): Promise<Producto> => {
+    const {nombre, precio, stock, categoria_id} = producto;
 
-      db.run(sql, [nombre, precio, stock, categoria_id], function(err) {
-        if(err) return reject(err);
-        resolve(this.lastID);
-      });
-    });
+    return await prisma.producto.create({
+      data: {
+        nombre, precio, stock, categoria_id
+      }
+    })
   },
 
-  // Consulta 4: actualizar los datos del producto
+  // Consulta 5: actualizar los datos del producto
   update: (id: number, datos: Producto): Promise<number> => {
     return new Promise((resolve, reject) => {
       const {nombre, precio, stock, categoria_id} = datos;
